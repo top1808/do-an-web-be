e57d8d8df1255a36a60680af307e182eff6ac2c9
top <top180802@gmail.com>
2023-11-27 17:19:37 +0700

    add crud order

--- src/app/admin.js
+++ src/app/admin.js
@@ -8,6 +8,7 @@ const customerRoute = require("../routes/admin/customer");
 const categoryRoute = require("../routes/admin/category");
 const productRoute = require("../routes/admin/product");
 const middlewareController = require("../controllers/middlewareController");
+const orderRoute = require('../routes/admin/order');
 
 router.use("/auth", authRoute);
 
@@ -16,6 +17,7 @@ router.use(middlewareController.checkPermission);
 
 router.use("/authorize", roleRoute);
 router.use("/user", userRoute);
+router.use("/order", orderRoute);
 router.use("/customer", customerRoute);
 router.use("/category", categoryRoute);
 router.use("/product", productRoute);
