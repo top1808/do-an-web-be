c71cd410dabb32c2955803143e93b35e8ed2cb0d
top1808 <88282244+top1808@users.noreply.github.com>
2023-11-28 00:57:26 +0700

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -117,6 +117,7 @@ const authController = {
           secure: true,
           sameSite: "strict",
           path: "/",
+          domain: process.env.NODE_ENV === 'development' ? '.localhost' : '.onrender.com'
         });
 
         res.status(200).send({ accessToken: newAccessToken });
